3f38d9e697a211ef06855ae779de2ce22b40ea0d
Anatolii Rotckin <rokkker@yandex.ru>
2023-02-09 17:39:12 +0200

    fix: исправить CORS

--- controllers/users.js
+++ controllers/users.js
@@ -50,6 +50,7 @@ const login = async (req, res, next) => {
         maxAge: 3600000 * 24,
         httpOnly: true,
         sameSite: 'None',
+        secure: true,
       }).json({ message: 'Вход выполнен успешно' });
     }
 
